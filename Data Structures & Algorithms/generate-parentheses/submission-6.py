5946e567723da1e235b7d5730b99f645d6c99631
nreh1r <87240487+nreh1r@users.noreply.github.com>
2026-07-22 12:38:24 -0600

    Add: generate-parentheses - submission-6

---
+++ Data Structures & Algorithms/generate-parentheses/submission-6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        self.res = []
+
+        def dfs(open_used, closed_used, substring):
+            if open_used == closed_used == n:
+                self.res.append(substring)
+                return
+            
+
+            if open_used < n:
+                dfs(open_used + 1, closed_used, f"{substring}(")
+            
+            if closed_used < open_used:
+                dfs(open_used, closed_used + 1, f"{substring})")
+            
+        dfs(0, 0, "")
+
+        return self.res
